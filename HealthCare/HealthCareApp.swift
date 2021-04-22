a409a74e17ee68f30cc0e2c2f354f57d63586566
akasoul <antonvoloshuk@gmail.com>
2021-04-22 21:53:24 +0300

    Initial Commit

---
+++ HealthCare/HealthCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCareApp.swift
+//  HealthCare
+//
+//  Created by Anton Voloshuk on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HealthCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
